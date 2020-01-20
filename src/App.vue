ac8cfd20dd1198c24091a62f0bfce9cdeca776da
bagus faisal <faizalbagus00@gmail.com>
2020-01-20 14:20:51 +0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #5ee689;">
+    <nav class="navbar navbar-expand-md navbar-light" style="background-color: #4db5fa;">
       <a class="navbar-brand fa fa-home" href="#"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,15 +9,18 @@
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item">
-            <a class="navbar-brand" href="#">Menu 1</a>
+            <router-link to="/"><a class="navbar-brand" href="#"> Repos</a></router-link>
           </li>
           <li class="nav-item">
-            <a class="navbar-brand" href="#">Menu 2</a>
+            <router-link to="/about"><a class="navbar-brand" href="#"> Profile</a></router-link>
           </li>
         </ul>
-        <a class="navbar-brand fa fa-sign-out" href="#"></a>
+        <a class="navbar-brand fa fa-sign-out" href="#"> Sign out</a>
       </div>
     </nav>
+      <div class="footer-copyright text-center py-3" style="color: #ffffff; background-color: #000000; position: fixed; bottom: 0; width: 100%">© 2020 Copyright:
+        <a href="#" style="color: #ffffff"> Lazizmu</a>
+      </div>
     <router-view>
     </router-view>
   </div>
