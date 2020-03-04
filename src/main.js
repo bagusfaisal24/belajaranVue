1594f6ef2f7264d772a88e4bad2042ab9e2e0796
bagus faisal <faizalbagus00@gmail.com>
2020-03-04 12:23:23 +0700

    implement data bootstrap vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './components/Home.vue'
-import About from './components/Member.vue'
+// import About from './components/member/Member.vue'
+import About from './components/member/Table.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 
 const routes = [
   { path: '/', component: Home },
