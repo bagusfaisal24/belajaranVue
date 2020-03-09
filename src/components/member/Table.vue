56dac44a7e1cb7dff1f6b5af06038abfbafae54b
bagus faisal <faizalbagus00@gmail.com>
2020-03-09 14:23:26 +0700

    fix table with api

--- src/components/member/Table.vue
+++ src/components/member/Table.vue
@@ -48,21 +48,27 @@
           :sort-desc.sync="sortDesc"
           :sort-direction="sortDirection"
           @filtered="onFiltered">
-<!--          <template v-slot="row">-->
-<!--            {{ row.name }}-->
-<!--          </template>-->
-<!--          <template v-slot:cell(actions)="row">-->
-<!--            <b-button size="sm" @click="row.toggleDetails">-->
-<!--              {{ row.detailsShowing ? 'Sembunyikan' : 'Lihat' }} Detail-->
-<!--            </b-button>-->
-<!--          </template>-->
-<!--          <template v-slot:row-details="row">-->
-<!--            <b-card>-->
-<!--              <ul>-->
-<!--                <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>-->
-<!--              </ul>-->
-<!--            </b-card>-->
-<!--          </template>-->
+          <!--          <template v-slot="row">-->
+          <!--            {{ row.name }}-->
+          <!--          </template>-->
+          <!--          <template v-slot:cell(actions)="row">-->
+          <!--            <b-button size="sm" @click="row.toggleDetails">-->
+          <!--              {{ row.detailsShowing ? 'Sembunyikan' : 'Lihat' }} Detail-->
+          <!--            </b-button>-->
+          <!--          </template>-->
+          <!--          <template v-slot:row-details="row">-->
+          <!--            <b-card>-->
+          <!--              <ul>-->
+          <!--                <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>-->
+          <!--              </ul>-->
+          <!--            </b-card>-->
+          <!--          </template>-->
+          <template slot="id" slot-scope="row">
+            {{ row.value.id }}
+            <template slot="name" slot-scope="row">
+              {{ row.value.name }}
+            </template>
+          </template>
         </b-table>
       </b-col>
       <b-col cols="12" class="my-1">
@@ -90,35 +96,11 @@ export default {
   name: 'Table',
   data () {
     return {
-      // members: [
-      //   { isActive: true, age: 40, name: { first: 'Dickerson' } },
-      //   { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
-      //   {
-      //     isActive: false,
-      //     age: 9,
-      //     name: { first: 'Mini', last: 'Navarro' }
-      //   },
-      //   { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
-      //   { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
-      //   { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
-      //   { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
-      //   {
-      //     isActive: true,
-      //     age: 87,
-      //     name: { first: 'Larsen', last: 'Shaw' }
-      //   },
-      //   { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
-      //   { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
-      //   { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
-      //   { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
-      // ],
       members: [],
-      // fields: [
-      //   { key: 'name', label: 'Nama', sortable: true, sortDirection: 'desc' },
-      //   { key: 'dobPlace', label: 'Tempat Lahir', sortable: true, class: 'text-center' },
-      //   { key: 'actions', label: 'Actions' }
-      // ],
-      fields: ['name', 'dobPlace', 'actions'],
+      fields: [
+        { key: 'id', label: 'id', class: 'text-center' },
+        { key: 'name', label: 'Nama', class: 'text-center' }
+      ],
       totalRows: 1,
       currentPage: 1,
       perPage: 20,
@@ -154,10 +136,19 @@ export default {
       this.infoModal.title = ''
       this.infoModal.content = ''
     },
+    // getDataMember () {
+    //   fetch('http://localhost:8080/v1/membership/member')
+    //     .then(r => r.json())
+    //     .then((data) => {
+    //       this.members = data
+    //     })
+    //     .catch(e => console.log(e))
+    // },
     getDataMember () {
-      MemberSvc
-        .getMember()
-        .catch(e => console.log(e))
+      MemberSvc.getMember()
+        .then((res) => {
+          this.members = res.data
+        }).catch(e => console.log(e))
     },
     onFiltered (filteredItems) {
       // Trigger pagination to update the number of buttons/pages due to filtering
